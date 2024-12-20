2b4e9182995698eda2411316238bee03032cdd4a
nopphanat01 <sspas401@gmail.com>
2024-12-20 16:15:59 +0700

    1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,14 +2,18 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main() {
+	double loan, interestRate, yearlyPayment;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> interestRate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> yearlyPayment;
+
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +22,27 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << fixed << setprecision(2);
+
+	int year = 1;
+	while (loan > 0) {
+		double interest = loan * (interestRate / 100.0);
+		double total = loan + interest ;
+		double payment = (total < yearlyPayment) ? total : yearlyPayment;
+		double newBalance = total - payment;	
+
+
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << interest;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << payment;
+		cout << setw(13) << left << newBalance;
+		cout << "\n";
+
+		loan = newBalance;
+		year++;
+	}	
 	
 	return 0;
 }
